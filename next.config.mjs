6da0dc037b59792b1574667416746531ddbe8016
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-06-15 21:40:53 +0530

    display movies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  // This configuration is specifically allowing images to be loaded from a particular domain (image.tmdb.org), which is essential when using Next.js's built-in Image Optimization feature.
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        pathname: "**",
+      },
+    ],
+  },
+};
 export default nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,34 @@
-import React from "react";
+import Results from "./components/Results";
 
-export default function page() {
-  return <div>Home</div>;
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending;";
+  const res = await new Promise((resolve) => {
+    setTimeout(async () => {
+      const res = await fetch(
+        `https://api.themoviedb.org/3${
+          genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
+        }?api_key=${API_KEY}&language=en-US&page=1`,
+        {
+          next: { revalidates: 10000 },
+        }
+      );
+      resolve(res); // When we call resolve with a value, the Promise's state changes from "pending" to "fulfilled".
+    }, 2000);
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const results = data.results;
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
