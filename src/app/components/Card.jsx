f1fd04d6fe8fe30b5bcb5ef097e3ebd9d7455e94
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-06-15 23:39:18 +0530

    image size variation fixed

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import { FiThumbsUp } from "react-icons/fi";
@@ -6,14 +7,16 @@ export default function Card({ result }) {
   return (
     <div className="group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200">
       <Link href={`/movie/${result.id}`}>
-        <Image
-          src={`https://image.tmdb.org/t/p/original/${
-            result.backdrop_path || result.poster_path
-          }`}
-          width={500}
-          height={300}
-          className="sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300"
-        />
+        <div className="relative w-full" style={{ paddingBottom: "56.25%" }}>
+          <Image
+            src={`https://image.tmdb.org/t/p/original/${
+              result.backdrop_path || result.poster_path
+            }`}
+            layout="fill"
+            objectFit="cover"
+            className="sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300"
+          />
+        </div>
         <div className="p-2">
           <p className="line-clamp-2 text-md">{result.overview}</p>
           <h2 className="font-bold truncate">{result.title || result.name}</h2>
